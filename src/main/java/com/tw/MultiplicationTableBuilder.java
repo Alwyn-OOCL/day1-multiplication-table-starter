8e97894a09b50fd0ae1993be40ef47bfeb4bb4ea
HEAL <HEAL@OOCL.COM>
2024-11-18 17:24:40 +0800

    [MultiplicationTable] feat: add "checkIfStartNumberLessThanOrEqualsToEndNumber" function

--- src/main/java/com/tw/MultiplicationTableBuilder.java
+++ src/main/java/com/tw/MultiplicationTableBuilder.java
@@ -6,10 +6,11 @@ public class MultiplicationTableBuilder {
         MultiplicationTableBuilder builder = new MultiplicationTableBuilder();
         int start = 2;
         int end = 4;
-        String multiplicationTable = builder.printMultiplicationTable(start, end);
+//        String multiplicationTable = builder.printMultiplicationTable(start, end);
 
 //        System.out.println(multiplicationTable);
-        System.out.println(builder.checkIfStartNumberAndEndNumberAreLegal(1, 4));
+//        System.out.println(builder.checkIfStartNumberAndEndNumberAreLegal(1, 4));
+        System.out.println(builder.checkIfStartNumberLessThanOrEqualsToEndNumber(4, 4));
     }
 
     public String printMultiplicationTable(int start, int end) {
@@ -19,5 +20,9 @@ public class MultiplicationTableBuilder {
 
     private boolean checkIfStartNumberAndEndNumberAreLegal(int start, int end) {
         return (start > 1 && start <= 1000) && (end > 1 && end <= 1000);
+    }
+
+    private boolean checkIfStartNumberLessThanOrEqualsToEndNumber(int start, int end) {
+        return start <= end;
     }
 }
